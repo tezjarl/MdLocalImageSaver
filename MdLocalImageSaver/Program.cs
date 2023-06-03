5f36452493c44d135a9918cd888674bf40f21a5c
Alexander <a.chertov@smartcat.ai>
2023-06-03 16:06:15 +0300

    Init commit

---
+++ MdLocalImageSaver/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
+{
+    Console.WriteLine("You should pass a md file name");
+    return;
+}
+var mdFilePath = args[0];
+var mdFileDir = Path.GetDirectoryName(mdFilePath)!; //считаем что null тут невозможен
+var mdFileContent = File.ReadAllText(mdFilePath);
+var client = new HttpClient();
+
+// Регулярка для поиска ссылок на изображения в формате ![alt text](url)
+var imageLinkRegex = new Regex(@"!\[.*?\]\((.*?)\)");
+var matches = imageLinkRegex.Matches(mdFileContent);
+
+foreach (Match match in matches)
+{
+    var imageUrl = match.Groups[1].Value;
+    var imageName = Path.GetFileName(imageUrl);
+
+    // Загружаем изображение
+    var imageBytes = await client.GetByteArrayAsync(imageUrl);
+    await File.WriteAllBytesAsync(Path.Combine(mdFileDir, imageName), imageBytes);
+
+    // Заменяем старую ссылку на новую
+    mdFileContent = mdFileContent.Replace(imageUrl, imageName);
+}
+
+// Сохраняем обновленный .md файл
+await File.WriteAllTextAsync(mdFilePath, mdFileContent);
